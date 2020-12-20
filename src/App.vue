a45bf3126831e66bcfae0bc07bf0218c642367ac
Chris Stroud <chris.stroud00@gmail.com>
2020-12-20 15:34:16 +0000

    add tailwindcss
    Had to add an older version due to issues relating to PostCSS v8.
    
    More info:
    https://tailwindcss.com/docs/installation#post-css-7-compatibility-build
    
    Change based on this comment:
    https://github.com/tailwindlabs/tailwindcss/discussions/2854#discussioncomment-136611

--- src/App.vue
+++ src/App.vue
@@ -62,13 +62,3 @@ export default class App extends Vue {
   }
 }
 </script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
