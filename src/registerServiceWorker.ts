6e6970b08c9a172b10f078326b19f6b73b9caabc
Chris Stroud <chris.stroud00@gmail.com>
2020-12-24 16:38:45 +0000

    attempt fix for service work always caching

--- src/registerServiceWorker.ts
+++ src/registerServiceWorker.ts
@@ -18,6 +18,7 @@ if (process.env.NODE_ENV === 'production') {
     },
     updatefound() {
       console.log('New content is downloading.');
+      setTimeout(() => window.location.reload(), 1000);
     },
     updated() {
       console.log('New content is available; please refresh.');
